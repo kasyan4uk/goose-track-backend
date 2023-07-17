b5dbe41e20f692ffb4dd4518b9ac7b9389c5dafd
Serhii Kasianchuk <k.kasan2016@gmail.com>
2023-07-17 03:48:34 +0300

    finish

--- models/contacts.js
+++ models/contacts.js
@@ -1,19 +1,62 @@
-// const fs = require('fs/promises')
+const fs = require('fs').promises;
+const path = require('path');
 
-const listContacts = async () => {}
+const { v4 } = require('uuid');
 
-const getContactById = async (contactId) => {}
+const contactsPath = path.join(__dirname, 'contacts.json');
 
-const removeContact = async (contactId) => {}
+const listContacts = async () => {
+  const contacts = await fs.readFile(contactsPath);
+  return JSON.parse(contacts);
+};
 
-const addContact = async (body) => {}
+const getById = async contactId => {
+  const contacts = await listContacts();
+  const result = contacts.find(contact => contact.id === contactId);
+  if (!result) {
+    return null;
+  }
+  return result;
+};
 
-const updateContact = async (contactId, body) => {}
+const addContact = async body => {
+  const contacts = await listContacts();
+  const result = { ...body, id: v4() };
+  contacts.push(result);
+  await writeContacts(contacts);
+  return result;
+};
+
+const updateContact = async (contactId, body) => {
+  const contacts = await listContacts();
+  const idx = contacts.findIndex(contact => contact.id === contactId);
+  if (idx === -1) {
+    return null;
+  }
+  contacts[idx] = { ...body, id: contactId };
+  await writeContacts(contacts);
+  return contacts[idx];
+};
+
+const removeContact = async contactId => {
+  const contacts = await listContacts();
+  const idx = contacts.findIndex(contact => contact.id === contactId);
+  if (idx === -1) {
+    return null;
+  }
+  const [result] = contacts.splice(idx, 1);
+  await writeContacts(contacts);
+  return result;
+};
+
+async function writeContacts(data) {
+  await fs.writeFile(contactsPath, JSON.stringify(data, null, 2));
+}
 
 module.exports = {
   listContacts,
-  getContactById,
+  getById,
   removeContact,
   addContact,
   updateContact,
-}
+};
---
+++ schemas/contacts.js
@@ -0,0 +1,19 @@
+const Joi = require('joi');
+
+const addSchema = Joi.object({
+  name: Joi.string()
+    .pattern(/^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$/)
+    .required(),
+  email: Joi.string()
+    .pattern(
+      /^([a-zA-Z0-9_\-.]+)@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.)|(([a-zA-Z0-9-]+\.)+))([a-zA-Z]{2,4}|[0-9]{1,3})(\]?)$/,
+    )
+    .required(),
+  phone: Joi.string()
+    .pattern(
+      /^(\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9})/,
+    )
+    .required(),
+});
+
+module.exports = addSchema;
