fbebbc9acad934d9f5d5d85894d26b0391bf145b
Serhii Kasianchuk <k.kasan2016@gmail.com>
2023-07-18 05:37:11 +0300

    fix: add validate on middleware

--- controllers/contacts.js
+++ controllers/contacts.js
@@ -2,8 +2,6 @@ const contacts = require("../models/contacts");
 
 const { HttpError, ctrlWrapper } = require('../helpers');
 
-const addSchema = require('../schemas/contacts')
-
 const getAll = async (req, res) => {
     const result = await contacts.listContacts();
     res.status(200).json(result);
@@ -20,25 +18,13 @@ const getById = async (req, res) => {
 };
 
 const add = async (req, res) => {
-    const { error } = addSchema.validate(req.body);
-    if (error) {
-        throw HttpError(400, "missing required name field");
-    }
     const result = await contacts.addContact(req.body);
     res.status(201).json(result);
-
 };
 
 const updateById = async (req, res) => {
-    const { error } = addSchema.validate(req.body);
-    if (error) {
-        throw HttpError(400, "missing fields");
-    }
     const { contactId } = req.params;
     const result = await contacts.updateContact(contactId, req.body)
-    if (!result) {
-        throw HttpError(404, 'Not found')
-    }
     res.json(result);
 };
 
--- routes/api/contacts.js
+++ routes/api/contacts.js
@@ -2,16 +2,20 @@ const express = require('express');
 
 const router = express.Router();
 
-const ctrl = require('../../controllers/contacts')
+const ctrl = require('../../controllers/contacts');
+
+const schemas = require('../../schemas/contacts');
+
+const { validation } = require('../../middleware');
 
 router.get('/', ctrl.getAll);
 
 router.get('/:contactId', ctrl.getById);
 
-router.post('/', ctrl.add);
+router.post('/', validation(schemas), ctrl.add);
 
 router.delete('/:contactId', ctrl.deleteById);
 
-router.put('/:contactId', ctrl.updateById);
+router.put('/:contactId', validation(schemas), ctrl.updateById);
 
-module.exports = router
+module.exports = router;
