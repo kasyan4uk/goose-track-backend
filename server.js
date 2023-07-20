f5658a36489ebd3dcb7db4cffc280f30bf78b841
Serhii Kasianchuk <k.kasan2016@gmail.com>
2023-07-20 05:12:15 +0300

    create conect db

--- server.js
+++ server.js
@@ -1,5 +1,16 @@
-const app = require('./app')
+const mongoose = require('mongoose');
 
-app.listen(3000, () => {
-  console.log("Server running. Use our API on port: 3000")
-})
+const app = require('./app');
+
+const DB_HOST = "mongodb+srv://kasyan4uk:vmzNI1tIEdNQoW1t@cluster0.lukhsbc.mongodb.net/db-contacts?retryWrites=true&w=majority";
+
+mongoose.connect(DB_HOST)
+  .then(() => {
+    app.listen(3000, () => {
+      console.log("Database connection successful")
+    })
+  })
+  .catch(error => {
+    console.log(error.message);
+    process.exit(1);
+  })
