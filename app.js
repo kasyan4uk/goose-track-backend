098603bdfd40f20b0ba64401c99a95e4cc108568
Konstantin Martynenko <konstantin.vents@gmail.com>
2023-08-24 23:40:02 +0300

    swagger.json added & app.js updated (swagger-doc connected)

--- app.js
+++ app.js
@@ -3,6 +3,10 @@ const logger = require('morgan')
 const cors = require('cors')
 require('dotenv').config();
 
+// підключення swagger документації 
+const swaggerUi = require("swagger-ui-express");
+const swaggerDocument = require("./swagger.json");
+
 const authRouter = require('./routes/api/users')
 
 const app = express()
@@ -12,6 +16,9 @@ const formatsLogger = app.get('env') === 'development' ? 'dev' : 'short'
 app.use(logger(formatsLogger))
 app.use(cors())
 app.use(express.json())
+
+// підключення swagger документації 
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
 app.use('/users', authRouter)
 
