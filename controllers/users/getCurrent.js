6c22d894c0119e0fbae4d7ed49871feb62c73e2c
alinabodnar1 <malina.bodnar@gmail.com>
2023-08-26 13:56:11 +0300

    fixed current

--- controllers/users/getCurrent.js
+++ controllers/users/getCurrent.js
@@ -1,10 +1,11 @@
 const getCurrent = async (req, res) => { 
-    const { email, subscription } = req.user;
-    console.log(req.user)
+    const { name, email } = req.user;
 
     res.status(200).json({
+      message: "Success",
+      userData:
+        name,
         email,
-        subscription,
     })
 };
 
