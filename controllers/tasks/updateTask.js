ae995b4d27101262fa9f2230e6b832abd98c87c3
Serhii Kasianchuk <k.kasan2016@gmail.com>
2023-09-05 01:24:08 +0300

    update updateTask.js

--- controllers/tasks/updateTask.js
+++ controllers/tasks/updateTask.js
@@ -3,11 +3,22 @@ const { Task } = require("../../models/task");
 
 const updateTask = async (req, res) => {
     const { id } = req.params;
+    const { _id: owner } = req.user;
+
+    const task = await Task.findById(id);
+
+    if (!task) {
+        throw HttpError(404, "Task not found");
+   }
+
+   if (task.owner.toString() !== owner.toString()) {
+        throw HttpError(403, "Unauthorized");
+   }
 
     const result = await Task.findByIdAndUpdate( id, req.body, {new: true});
 
     if(!result) {
-        throw HttpError(404, "Not found");
+        throw HttpError(404, "Task not found");
     };
 
     res.status(200).json({
