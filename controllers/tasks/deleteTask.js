0f47444e90d0feaf1cae19b08f1cdeb6971d11e3
Serhii Kasianchuk <k.kasan2016@gmail.com>
2023-09-05 01:16:14 +0300

    update deleteTask.js

--- controllers/tasks/deleteTask.js
+++ controllers/tasks/deleteTask.js
@@ -3,17 +3,31 @@ const { Task } = require("../../models/task");
 
 const deleteTaskById = async (req, res) => {
    const { id } = req.params;
-   const { owner } = req.user;
+   const { _id: owner } = req.user;
+
+   const task = await Task.findById(id);
+
+   if (!task) {
+        throw HttpError(404, "Task not found");
+   }
+
+   if (task.owner.toString() !== owner.toString()) {
+        throw HttpError(403, "Unauthorized");
+   }
 
    const result = await Task.findByIdAndDelete(id);
 
-   if(!result || !owner) {
-        throw HttpError(404, "Not found")
+   if (!result) {
+        throw HttpError(404, "Task not found");
    }
 
    res.status(200).json({
-        message: "Task deleted successfully"
-})
+      message: "Task deleted successfully"
+   })
 };
 
 module.exports = deleteTaskById;
+
+
+
+      
